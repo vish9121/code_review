af9956628b766650f9c224b7de729722ebcb5e50
Vishal namdev <namdeovishal12@gmail.com>
2025-05-06 00:31:40 +0530

    backend

---
+++ BackEnd/server.js
@@ -0,0 +1,6 @@
+require('dotenv').config()
+const app = require('./src/app');
+
+app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
---
+++ BackEnd/src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const aiRoutes = require("./routes/ai.routes")
+const app = express();
+const cors = require('cors')
+
+// your middleware/routes here
+app.use(cors())
+app.use(express.json())
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+app.use('/ai',aiRoutes)
+module.exports = app;
